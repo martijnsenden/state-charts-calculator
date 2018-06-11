32ce68df8a093a50c51625f80fbcb393393c1054
Martijn Senden <martijn@instantmagazine.com>
2018-06-11 11:12:47 +0200

    - Setup Fusebox

---
+++ fuse.js
@@ -0,0 +1,13 @@
+const { FuseBox, WebIndexPlugin } = require("fuse-box");
+const fuse = FuseBox.init({
+	homeDir : "src",
+	target : 'browser@es6',
+	output : "dist/$name.js",
+	plugins : [
+		WebIndexPlugin()
+	],
+	useTypescriptCompiler : true,
+});
+fuse.dev(); // launch http server
+fuse.bundle("app").instructions(" > index.ts").hmr().watch()
+fuse.run();
